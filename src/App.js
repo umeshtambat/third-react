f70e74f75eb58482cd6d8725e12a77d086e0bc6d
Umesh <Umeshtambat55@gmail.com>
2023-06-14 10:00:30 +0530

    function structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 function App() {
+  return (
+    <>
+      <h1>Counter App</h1>
+      <CounterApp />
+    </>
+  );
+}
+
+//user
+//userefineTag
+function CounterApp() {
+  //data menbers
+  let counter = 1;
+  // let buttonTitle = "Like Me";
+  let id = 100;
+  let str = "Hello World";
+  let active = true;
+
+  //Member function :: Optional
+  let likeMeAction = () => {};
+  let dislikeMeAction = () => {};
+
+  //JSX :: VIEW :: USER-INTERFACE :: Required
 
   return (
     <>
-    <h1>Counter App</h1>
-    <CounterApp />
-
+      <h1>{counter}</h1>
+      <input type="button" value="Like Me!" />
     </>
-  )
+  );
 }
 
-function CounterApp() {
-  return(
-    <>
-    <h1>1</h1>
-    <input type="button"  value="Like Me!"/>
-    </>
-  )
-}
-
-
-  export default App;
+export default App;
