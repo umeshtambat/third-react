b21bf09c198dc7ebde087fbdeda11095f3a3ef03
Umesh <Umeshtambat55@gmail.com>
2023-06-14 19:51:31 +0530

    counter app developed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import { useState } from "react";
-
 function App() {
   return (
     <>
-      <h1>Counter App</h1>
+      <h1>Couter App</h1>
+      <CounterApp />
       <CounterApp />
     </>
   );
 }
 
-// UserDefineTag
 function CounterApp() {
-  // Data Member :: stateless/ stateful
-  // let counter = 1; // stateless
-  let [counter, setCounter] = useState(1); // to create stateful variable + this will help to perform dom operation.
+  // let counter = 1;   //stateless
 
-  // Member Function
+  //  let var1 = useState(1);
+  // console.log(var1);
+
+  // let list = useState(1);
+  // let[counter, setCounter]= list;
+
+  let [counter, setCounter] = useState(1);
+
   let likeMeAction = () => {
-    counter = counter + 1;
-    console.log("I m button click", counter);
+    counter += 1;
+    console.log(counter);
 
-    // Asking REACT TO PERFOMR DOM OEPRATION
-    // Calling setCounter => DOM Operation
+    //asking react to perform DOM operation
     setCounter(counter);
   };
-
   return (
     <>
-      <h1>{counter}</h1>
-      <input type="button" value="Like Me" onClick={likeMeAction} />
+      <h5>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores quam
+        reiciendis nisi, sunt dolore delectus nesciunt minus expedita pariatur
+        magni.
+      </h5>
+      <h5>
+        {counter} <input type="button" value="like Me" onClick={likeMeAction} />
+      </h5>
     </>
   );
 }
