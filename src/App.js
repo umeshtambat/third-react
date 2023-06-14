82ee9a13054b82ad5f47d658fa4aeacad6a7e273
Umesh <Umeshtambat55@gmail.com>
2023-06-14 09:27:02 +0530

    breaking task into smaller task

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function AppHeader() {
 function AppBody() {
   return (
     <>
-      <div className="alert-success fs-3 mb-0">
+      <div className="alert alert-success fs-3 mb-0">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
         excepturi assumenda eius possimus, reprehenderit autem obcaecati
         exercitationem tenetur quae deserunt quod eveniet qui voluptatum ut
