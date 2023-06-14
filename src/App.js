38995eeff5ea55a82b81de62772547d5ca19606c
Umesh <Umeshtambat55@gmail.com>
2023-06-14 09:14:48 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@
 function App() {
   return (
     <>
-      
       {/* <AppHeader></AppHeader> */}
-      <AppHeader/>
-      <AppBody/>
-      <AppFooter/>
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
     </>
   );
 }
@@ -14,28 +13,34 @@ function App() {
 //user defined Tag: <AppHeader/>
 function AppHeader() {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <div className="row bg-success text-light">
+     
+       
+        {/* <h1 className="bg-dark  trxt-light p-3">Header</h1> */}
+        <div className="col">Home</div>
+        <div className="col">Project</div>
+        <div className="col">AboutUs</div>
+      
+    </div>
   );
 }
 
 //user defined Tag: <AppBody/>
 function AppBody() {
-  return(
+  return (
     <>
-    <h1>App Body</h1>
+      <h1>App Body</h1>
     </>
-  )
+  );
 }
 
 //user defined Tag: <AppFooter/>
 function AppFooter() {
   return (
     <>
-    <h1>App Footer</h1>
+      <h1>App Footer</h1>
     </>
-  )
+  );
 }
 
 export default App;
